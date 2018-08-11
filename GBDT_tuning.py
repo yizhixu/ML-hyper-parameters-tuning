367c7503d6ce6ae5fcc2c73a4a85e5e157b6da2c
orange-xyz <1213772031@qq.com>
2018-08-11 21:59:40 +0800

    minor

--- GBDT_tuning.py
+++ GBDT_tuning.py
@@ -16,7 +16,7 @@ np.random.seed(1024)
 
 class GBDT_tuning:
 
-	def __init__(self, X, y, cv=5, n_jobs=30, random_state=2018, model_type='classification', scoring='accuracy'):
+	def __init__(self, X, y, cv=5, n_jobs=30, random_state=2018, model_type='classification', scoring=['accuracy', 'roc_auc', 'f1']):
 		self.X = X
 		self.y = y
 		self.cv = cv
@@ -28,16 +28,16 @@ class GBDT_tuning:
 	def gridsearch(self, param_grid):
 		if(self.model_type == 'classification' or self.model_type == 'clf'):
 			estimator = GradientBoostingClassifier(learning_rate=0.1, min_samples_split=1000,
-                                				   min_samples_leaf=20, max_features='sqrt', subsample=0.8, random_state=self.random_state)
+                                				   min_samples_leaf=20, max_features='sqrt', subsample=0.8, random_state=self.random_state, )
 		elif(self.model_type == 'regression' or self.model_type == 'reg'):
 			estimator = GradientBoostingRegressor(learning_rate=0.1, min_samples_split=1000,
                                 				  min_samples_leaf=20, max_features='sqrt', subsample=0.8, random_state=self.random_state)
 
 		gsearch = GridSearchCV(estimator=estimator, 
                        		   param_grid=param_grid, scoring=self.scoring, cv=self.cv, n_jobs = self.n_jobs, 
-                       		   verbose=2)
+                       		   refit='accuracy', verbose=2)
 		gsearch.fit(self.X, self.y)
-		return gsearch.grid_scores_, gsearch.best_params_, gsearch.best_score_
+		return gsearch.best_params_, gsearch.best_score_, np.array(gsearch.cv_results_['mean_test_roc_auc']).mean(), np.array(gsearch.cv_results_['mean_test_f1']).mean()
 
 	def randomsearch(self, param_distributions):
 		if(self.model_type == 'classification' or self.model_type == 'clf'):
@@ -51,7 +51,7 @@ class GBDT_tuning:
                        				  param_distributions=param_distributions, scoring=self.scoring, cv=self.cv, n_jobs = self.n_jobs, 
                        				  n_iter=20, verbose=2)
 		rsearch.fit(self.X, self.y)
-		return rsearch.grid_scores_, rsearch.best_params_, rsearch.best_score_
+		return rsearch.best_params_, rsearch.best_score_
 
 	def search(self, search_type, params):
 		if(search_type == 'grid'):
@@ -66,14 +66,14 @@ class GBDT_tuning:
 		return self.search(search_type, params)
 
 	def step1(self, search_type, params):
-		params['max_depth'] = range(5, 15, 2)
+		params['max_depth'] = range(3, 10, 2)
 		params['min_samples_split'] = (len(self.y)*np.arange(0.002, 0.01, 0.002)).astype(int)
 
 		return self.search(search_type, params)
 
 	def step2(self, search_type, params):
 		params['min_samples_split'] = (len(self.y)*np.arange(0.01, 0.02, 0.002)).astype(int)
-		params['min_samples_split'] = (len(self.y)/np.arange(30, 71, 10)).astype(int)
+		params['min_samples_leaf'] = (len(self.y)/np.arange(30, 71, 10)).astype(int)
 
 		return self.search(search_type, params)
 
@@ -98,11 +98,12 @@ class GBDT_tuning:
 		start_time = time.time()
 		params = dict()
 		for i in range(steps):
-			_0, best_params_, best_scores_ = eval('self.step%d(\'grid\', params)'%(i))
+			best_params_, best_scores_, auc, f1= eval('self.step%d(\'grid\', params)'%(i))
 			for key, value in best_params_.items():
 				if(type(value) == str):
 					params[key] = eval('[\'%s\']'%(value))
 				else:
 					params[key] = eval('[%s]'%(value))
-		print(params, best_scores_)
+		print(params, best_scores_, auc, f1)
 		print('finish tuning GBDT model in %d seconds'%(time.time() - start_time))
+		return best_scores_, auc, f1
